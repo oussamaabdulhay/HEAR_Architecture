3e869c53157cc3cf7614d17998343712d3d7eee7
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-01-12 11:13:08 +0400

    fixed the kalman filter

--- include/HEAR_math/KalmanFilter.hpp
+++ include/HEAR_math/KalmanFilter.hpp
@@ -11,12 +11,13 @@ class KalmanFilter : public Block {
     private:
         Port* _input_port_0;
         Port* _input_port_1;
+        Port* _input_port_2;
         Port* _output_port_0;
         Port* _output_port_1;
         Port* _output_port_2;
-        float _dt = 0.005;
-        float _war_w = 0.5;
-        float _R_pos;
+        float _dt = float(1/200.f);
+        float _war_w = 0.2;
+        float _R_pos = 0.001;
         float _pos_val=0;
         Eigen::Matrix<float, 3, 1> _x, _G;
         Eigen::Matrix<float, 3, 3> _P, _F, _Q;
@@ -24,7 +25,7 @@ class KalmanFilter : public Block {
         void resetFilter();
 
     public:
-        enum ports_id {IP_0_ACC, IP_1_POS, OP_0_POS, OP_1_VEL, OP_2_BIAS};
+        enum ports_id {IP_0_ACC, IP_1_POS, IP_2_RESET, OP_0_POS, OP_1_VEL, OP_2_BIAS};
         void process(DataMsg* t_msg, Port* t_port);
         void setTimeStep(float);
         void doPredictionStep(float);
