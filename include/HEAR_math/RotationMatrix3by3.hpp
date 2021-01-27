c9145849f93c48b70ac38d70d8b35d3f54b871e0
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-01-27 14:24:59 +0400

    pushing to main repository ( checked the rotation matrices)

--- include/HEAR_math/RotationMatrix3by3.hpp
+++ include/HEAR_math/RotationMatrix3by3.hpp
@@ -5,9 +5,9 @@
 
 class RotationMatrix3by3 : public Matrix3by3 {
     private:
-       Eigen::MatrixXd _m;
+        Eigen::Matrix<float, 3, 3> _m;
     public:
         RotationMatrix3by3();
         ~RotationMatrix3by3();
-        Eigen::MatrixXd Update(Vector3D<float>);
+        Eigen::Matrix<float, 3, 3> Update(Vector3D<float>);
 };
