d330d867a49734a1d36134a439f3433d3096e398
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-02-11 14:38:24 +0400

    Using the ENU rotation matrix now

--- src/RotationMatrix3by3.cpp
+++ src/RotationMatrix3by3.cpp
@@ -20,17 +20,17 @@ Eigen::Matrix<float, 3, 3> RotationMatrix3by3::Update(Vector3D<float> t_input)
     float sy = sin(t_input.z);
     float cy = cos(t_input.z);
     
-    Matrix3by3::v1.x = cp * cy;
-    Matrix3by3::v1.y = sr * sp * cy - cr * sy;
-    Matrix3by3::v1.z = cr * sp * cy + sr * sy;
+    Matrix3by3::v1.x =cy * cp;
+    Matrix3by3::v1.y =sy * cp;
+    Matrix3by3::v1.z =sp * -1;
 
-    Matrix3by3::v2.x = cp * sy;
-    Matrix3by3::v2.y = sr * sp * sy + cr * cy;
-    Matrix3by3::v2.z = cr * sp * sy - sr * cy;
+    Matrix3by3::v2.x =cy * sp * sr - sy * cr;
+    Matrix3by3::v2.y =sy * sp * sr + cy * cr;
+    Matrix3by3::v2.z =cp * sr;
 
-    Matrix3by3::v3.x = sp * -1;
-    Matrix3by3::v3.y = cp * sr;
-    Matrix3by3::v3.z = cp * cr;
+    Matrix3by3::v3.x =cy * sp * cr + sy * sr;
+    Matrix3by3::v3.y =sy * sp * cr - cy * sr;
+    Matrix3by3::v3.z =cp * cr;
 
     /* 
     
