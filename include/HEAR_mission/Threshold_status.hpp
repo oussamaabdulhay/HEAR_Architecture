7280c25da04befe4d57a6d292527e4807145082b
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-03-10 13:57:18 +0400

    added threshold status class

---
+++ include/HEAR_mission/Threshold_status.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "HEAR_core/InputPort.hpp"
+#include "HEAR_core/OutputPort.hpp"
+#include "HEAR_core/Block.hpp"
+#include "HEAR_msg/Vector3DMsg.hpp"
+#include "HEAR_msg/FloatMsg.hpp"
+
+class Threshold_status : public Block {
+private:
+    Port* _input_port_0;
+    Port* _output_port_0; 
+    float threshold_position;
+    float threshold_velocity;
+    float dt;
+    int trigger_value;
+    float old_value; 
+public:
+    enum ports_id {IP_0 ,OP_0_HOV_TRACK};
+    void process(DataMsg* t_msg, Port* t_port);
+    void runTask(float);
+    Threshold_status(float, float, float);
+    ~Threshold_status();
+};
---
+++ src/Threshold_status.cpp
@@ -0,0 +1,53 @@
+#include "HEAR_mission/Threshold_status.hpp"
+
+Threshold_status::Threshold_status(float _threshold_pos,float _threshold_vel,float _dt ) {
+    _input_port_0 = new InputPort(ports_id::IP_0, this);
+    _output_port_0 = new OutputPort(ports_id::OP_0_HOV_TRACK, this);
+    _ports = {_input_port_0, _output_port_0};
+
+    threshold_position = _threshold_pos;
+    threshold_velocity = _threshold_vel;
+    dt = 1./_dt;
+}
+
+Threshold_status::~Threshold_status() {
+
+}
+
+void Threshold_status::process(DataMsg* t_msg, Port* t_port) {
+
+    if(t_port->getID() == ports_id::IP_0){
+        FloatMsg* float_msg = (FloatMsg*)t_msg;
+        float data = float_msg->data;
+
+        runTask(data);
+        old_value = data;
+     }
+     
+
+}
+
+void Threshold_status::runTask(float position) {
+
+    float relative_position; 
+    relative_position = abs(position);
+    
+
+    float relative_velocity; 
+    relative_velocity = abs(position - old_value)/dt;
+
+        
+    if(relative_position>threshold_position && relative_velocity>threshold_velocity)
+    {
+        FloatMsg trigger_msg;
+        trigger_msg.data = 2.5;
+        this->_output_port_0->receiveMsgData((DataMsg*) &trigger_msg);
+        
+    }
+    else if(relative_position<threshold_position && relative_velocity<threshold_velocity)
+    {
+        FloatMsg trigger_msg;
+        trigger_msg.data = 1;
+        this->_output_port_0->receiveMsgData((DataMsg*) &trigger_msg);
+    }
+}
