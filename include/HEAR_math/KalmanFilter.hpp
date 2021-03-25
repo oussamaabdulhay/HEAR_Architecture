b4bac1abb77d3b321064fb32bcc88d882723b095
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-03-25 15:30:17 +0400

    .

--- include/HEAR_math/KalmanFilter.hpp
+++ include/HEAR_math/KalmanFilter.hpp
@@ -16,8 +16,8 @@ class KalmanFilter : public Block {
         Port* _output_port_1;
         Port* _output_port_2;
         float _dt = float(1/200.f);
-        float _war_w = 0.1;
-        float _R_pos = 0.001;
+        float _war_w = 0.01;
+        float _R_pos = 0.01;
         float _pos_val=0;
         Eigen::Matrix<float, 3, 1> _x, _G;
         Eigen::Matrix<float, 3, 3> _P, _F, _Q;
